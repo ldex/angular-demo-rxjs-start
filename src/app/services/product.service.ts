00381d0a050bf021531c7477b9de55432e645153
ldex <laurent.duveau+git@gmail.com>
2024-01-19 14:19:31 -0500

    Changed API

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, delay, shareReplay, tap, first, map, mergeAll, BehaviorSubject, switchMap, of, filter } from 'rxjs';
+import {
+  Observable,
+  catchError,
+  delay,
+  shareReplay,
+  tap,
+  first,
+  map,
+  mergeAll,
+  BehaviorSubject,
+  switchMap,
+  of,
+  filter,
+} from 'rxjs';
 import { Product } from '../products/product.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  private baseUrl = 'https://storerestservice.azurewebsites.net/api/products/';
+  private baseUrl: string = `${environment.apiUrl}/products`;
   products$: Observable<Product[]>;
 
   constructor(private http: HttpClient) {
@@ -16,23 +29,27 @@ export class ProductService {
   }
 
   initProducts() {
-    let url:string = this.baseUrl + `?$orderby=ModifiedDate%20desc`;
+    const params = {
+      _sort: 'modifiedDate',
+      _order: 'desc',
+    };
 
-    this.products$ = this
-                      .http
-                      .get<Product[]>(url)
-                      .pipe(
-                        delay(1500),
-                        tap(console.table)
-                      );
+    const options = {
+      params: params,
+    };
+
+    this.products$ = this.http
+      .get<Product[]>(this.baseUrl, options)
+      .pipe(delay(1500), tap(console.table));
   }
 
   insertProduct(newProduct: Product): Observable<Product> {
+    newProduct.modifiedDate = new Date();
     return this.http.post<Product>(this.baseUrl, newProduct).pipe(delay(2000));
   }
 
   deleteProduct(id: number): Observable<any> {
-    return this.http.delete(this.baseUrl + id);
+    return this.http.delete(this.baseUrl + '/' + id);
   }
 
   resetList() {
