2015d17c3315c7cadd7883b54c744488022c2f51
ldex <laurent.duveau+git@gmail.com>
2025-11-24 20:05:41 -0500

    Deleting old files

--- src/app/custom-route-reuse-strategy.ts
+++
@@ -1,7 +0,0 @@
-import {ActivatedRouteSnapshot, BaseRouteReuseStrategy } from '@angular/router';
-
-export class CustomRouteReuseStrategy extends BaseRouteReuseStrategy {
-  public shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return false;
-  }
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://671d383409103098807c943e.mockapi.io/api'
-};
